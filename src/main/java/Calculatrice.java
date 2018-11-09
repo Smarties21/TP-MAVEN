2a805ca032650179bef4b929de28f0f2cc5c30bc
imane <imanedjt@gmail.com>
2018-11-09 15:46:03 +0100

    test1

---
+++ src/main/java/Calculatrice.java
@@ -0,0 +1,15 @@
+
+public class Calculatrice {
+	
+	public int Division(int a, int b) {
+		
+		int result;
+	     if (b > 0)
+	         result = 1;
+	      else
+	    	  result = (a / b);
+	     //System.out.println(result);
+	     return result;
+	}
+	
+}
